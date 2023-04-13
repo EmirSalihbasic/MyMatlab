386fe15c036609bd647acae160c6e22fd02b41ae
EmirSalihbasic <emirsalihbasic10@gmail.com>
2023-04-13 02:12:47 +0200

    Add files via upload

---
+++ MyPitagora.m
@@ -0,0 +1,8 @@
+%Pitagoras theorem example
+function [c] = MyPitagora(a,b)       
+%Calculate C from pythagoras
+%   I love right triangles
+c = sqrt ((a^2) + (b^2));
+end
+
+%to read this we have to write hyp = MyPitagora (num1,num2) in command line
---
+++ importcsv.m
@@ -0,0 +1,5 @@
+%Importing and reading the data
+%in the files right click and choose the option to import a file.
+
+MyData = readtable ("csv.import.csv");
+
